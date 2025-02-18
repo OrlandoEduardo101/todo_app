83bb4664a9084aa050c48a4839f1784b00f014f7
Orlando Eduardo Pereira <orlando@fteam.dev>
2025-02-18 11:15:39 -0300

    Refactor localization handling and update app label

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Importe as traduções geradas
-import 'todo_list_screen.dart'; 
+import 'todo_list_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To-Do List',
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: TodoListScreen(),
-    );
+    if (!kIsWeb) {
+      final String defaultLocale = Platform.localeName; // Returns locale string in the form 'en_US'
+      log(defaultLocale, name: 'defaultLocale');
+      changeLocale(Locale(defaultLocale.split('_').first));
+    }
+    return ListenableBuilder(
+        listenable: localeNotifier,
+        builder: (context, _) {
+          return MaterialApp(
+            title: 'To Do',
+            locale: localeNotifier.value,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            home: TodoListScreen(),
+          );
+        });
   }
+}
+
+final localeNotifier = ValueNotifier<Locale>(const Locale('en'));
+
+void changeLocale(Locale locale) {
+  localeNotifier.value = locale;
 }
--- lib/todo_list_screen.dart
+++ lib/todo_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/main.dart';
 import 'package:todo_app/todo_model.dart';
 import 'package:todo_app/todo_repository.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -15,19 +16,17 @@ class _TodoListScreenState extends State<TodoListScreen> {
   final TodoRepository _repository = TodoRepository();
   final List<Todo> _todos = [];
   final TextEditingController _textController = TextEditingController();
-  Locale _locale = const Locale('en'); // Idioma padrão: inglês
 
   void _changeLanguage(Locale locale) {
-    setState(() {
-      _locale = locale;
-    });
+    changeLocale(locale);
   }
 
   final String picpayUrl = "https://picpay.me/orlandoeduardo.pereira/5";
 
   void _openPicPay() async {
     final Uri url = Uri.parse(picpayUrl);
-    if (await canLaunchUrl(url)) {
+    var bool = await canLaunchUrl(url);
+    if (bool) {
       await launchUrl(url, mode: LaunchMode.externalApplication);
     } else {
       debugPrint("Não foi possível abrir o link");
@@ -37,96 +36,92 @@ class _TodoListScreenState extends State<TodoListScreen> {
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context);
-    return MaterialApp(
-      locale: _locale, // Define o idioma selecionado
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(AppLocalizations.of(context)!.appTitle),
-          actions: [
-            PopupMenuButton<Locale>(
-              onSelected: _changeLanguage,
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem(
-                    value: const Locale('en'),
-                    child: Text(AppLocalizations.of(context)!.english),
-                  ),
-                  PopupMenuItem(
-                    value: const Locale('es'),
-                    child: Text(localizations!.spanish),
-                  ),
-                  PopupMenuItem(
-                    value: const Locale('pt'),
-                    child: Text(AppLocalizations.of(context)!.portuguese),
-                  ),
-                ];
-              },
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _textController,
-                      decoration: InputDecoration(
-                        hintText: AppLocalizations.of(context)!.addTaskHint,
-                      ),
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(localizations?.appTitle ?? ''),
+        actions: [
+          PopupMenuButton<Locale>(
+            onSelected: _changeLanguage,
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: const Locale('en'),
+                  child: Text(AppLocalizations.of(context)!.english),
+                ),
+                PopupMenuItem(
+                  value: const Locale('es'),
+                  child: Text(localizations!.spanish),
+                ),
+                PopupMenuItem(
+                  value: const Locale('pt'),
+                  child: Text(AppLocalizations.of(context)!.portuguese),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: AppLocalizations.of(context)?.addTaskHint,
                     ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      if (_textController.text.isNotEmpty) {
-                        _addTodo(_textController.text);
-                        _textController.clear();
-                      }
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    if (_textController.text.isNotEmpty) {
+                      _addTodo(_textController.text);
+                      _textController.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                final todo = _todos[index];
+                return ListTile(
+                  title: Text(
+                    todo.title,
+                    style: TextStyle(
+                      decoration: todo.isCompleted ? TextDecoration.lineThrough : null,
+                    ),
+                  ),
+                  leading: Checkbox(
+                    value: todo.isCompleted,
+                    onChanged: (value) {
+                      _toggleTodoCompletion(index);
                     },
                   ),
-                ],
-              ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      _deleteTodo(index);
+                    },
+                  ),
+                );
+              },
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _todos.length,
-                itemBuilder: (context, index) {
-                  final todo = _todos[index];
-                  return ListTile(
-                    title: Text(
-                      todo.title,
-                      style: TextStyle(
-                        decoration: todo.isCompleted ? TextDecoration.lineThrough : null,
-                      ),
-                    ),
-                    leading: Checkbox(
-                      value: todo.isCompleted,
-                      onChanged: (value) {
-                        _toggleTodoCompletion(index);
-                      },
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        _deleteTodo(index);
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: _openPicPay,
-          icon: const Icon(Icons.coffee),
-          label: Text(localizations?.buy_me_a_coffee ?? ''),
-        ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _openPicPay,
+        icon: const Icon(Icons.coffee),
+        label: Text(localizations?.buy_me_a_coffee ?? ''),
       ),
     );
   }
